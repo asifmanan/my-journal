de281a08857346ec273b84a467e8648f1991e2f2
asifmanan <asifmanan@gmail.com>
2024-04-25 23:22:43 +0100

    Creating basic structure, in progress

--- src/main/java/io/myjournal/controllers/IndexController.java
+++ src/main/java/io/myjournal/controllers/IndexController.java
@@ -1,12 +1,33 @@
 package io.myjournal.controllers;
 
+import io.myjournal.models.JournalEntry;
+import io.myjournal.models.JournalEntryCollection;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
 
 public class IndexController {
-    @FXML private Label titleLabel;
-    @FXML private Label timeLabel;
-    @FXML Label datelabel;
+    @FXML private ListView<String> entriesListView;
+    private JournalEntryCollection journalEntryCollection = new JournalEntryCollection();
+    private ObservableList<String> entryTitles = FXCollections.observableArrayList();
+
+//    @FXML private Label titleLabel;
+//    @FXML private Label timeLabel;
+//    @FXML Label datelabel;
+    public IndexController(){
+        journalEntryCollection.addPropertyChangeListener(evt -> {
+            if("addEntry".equals(evt.getPropertyName())) {
+                JournalEntry journalEntry = (JournalEntry) evt.getNewValue();
+                entryTitles.add(journalEntry.getTitle());
+            }
+        });
+        journalEntryCollection.getJournalEntryList().forEach(entry -> entryTitles.add(entry.getTitle()));
+    }
+    @FXML
+    public void initialize(){
+        entriesListView.setItems(entryTitles);
+    }
 
 
 }
