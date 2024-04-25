514a3acc898b84ab6ce5bea57fa74afd86706c50
asifmanan <asifmanan@gmail.com>
2024-04-25 01:11:05 +0100

    Updated project structure

--- src/main/java/io/myjournal/controllers/HelloController.java
+++ src/main/java/io/myjournal/controllers/HelloController.java
@@ -1,4 +1,4 @@
-package io.myjournal;
+package io.myjournal.controllers;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,10 @@
-module io.myjournal.myjournal {
+module io.myjournal {
     requires javafx.controls;
     requires javafx.fxml;
 
 
     opens io.myjournal to javafx.fxml;
-    exports io.myjournal;
+    exports io.myjournal to javafx.graphics;
+    exports io.myjournal.controllers to javafx.graphics;
+    opens io.myjournal.controllers to javafx.fxml;
 }
